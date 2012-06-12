fc90b2579b31f4add49a563c6821053e938fa9a1
ttakahashi <ttakahashi@up-frontier.jp>
2012-06-12 16:04:53 +0900

    test succeed

--- pic_zoomer.rb
+++ pic_zoomer.rb
@@ -23,74 +23,75 @@ class Transform
   TATE = 			21
   SQUARE =			22
 
-
-
-def priority_long (edge, posw, outh, outw, inw, inh, posh)
+  def priority_long (edge, posw, outh, outw, inw, inh, posh)
         #-----位置の前処理と拡大処理-----
-  if edge == YOKO then
-    posw = NONE
-    outh = outw.to_f / inw.to_f * inh.to_f
-  else
-    posh = NONE
-    outw = outh.to_f / inh.to_f * inw.to_f
-  end
-  inx = iny = outx = outy = 0
-  ret = Hash.new
-  ret.default = 0
-  ret.store("inx", inx)
-  ret.store("iny", iny)
-  ret.store("outx", inw)
-  ret.store("outy", inh)
-  ret.store("edge", edge)
-  ret.store("posw", posw)
-  ret.store("outh", outh)
-  ret.store("posh", posh)
-  return ret
-end
-
-def priority_short (edge, posh, posw, inx, inw, outw, iny, inh, outh, outx, outy)
-  #-----位置の前処理-----
-  if edge == YOKO then
-    posh = NONE
-    posw = NONE if posw == MID_W
-  else
-    posw = NONE
-    posh = NONE if posh == MID_H
-  end
-  #-----拡大処理-----
-  if edge ==YOKO then
-    inx = (outw.to_f - inw.to_f) / 2.0
-    inw_tmp = inw if posw == RIGHT
-    inw = outw
-    iny = 0
-  elsif edge == TATE then
-    inx = 0
-    iny = (inh.to_f - outh.to_f) / 2.0
-    inh = outh
-  else
-    inx = 0
-    inw = outw
-    iny = (inh.to_f - outh.to_f) / 2.0
-    inh = outh
-  end
-    outx = outy = 0
+    if edge == YOKO then
+      posw = NONE
+      outh = outw.to_f / inw.to_f * inh.to_f
+    else
+      posh = NONE
+      outw = outh.to_f / inh.to_f * inw.to_f
+    end
+    inx = iny = outx = outy = 0
     ret = Hash.new
     ret.default = 0
-    ret.store("edge", edge)
-    ret.store("posh", posh)
-    ret.store("posw", posw)
     ret.store("inx", inx)
-    ret.store("inw", inw)
-    ret.store("outw", outw)
     ret.store("iny", iny)
-    ret.store("inh", inh)
-    ret.store("outh", outh)
     ret.store("outx", outx)
     ret.store("outy", outy)
-    ret.store("inw_tmp", inw_tmp)
-  return ret
+    ret.store("edge", edge)
+    ret.store("outh", outh)
+    ret.store("posw", posw)
+    ret.store("posh", posh)
+    ret.store("inw", inw)
+    ret.store("inh", inh)
+    ret.store("outw", outw)
+    return ret
   end
-  
+
+  def priority_short (edge, posh, posw, inx, inw, outw, iny, inh, outh, outx, outy)
+    #-----位置の前処理-----
+    if edge == YOKO then
+      posh = NONE
+      posw = NONE if posw == MID_W
+    else
+      posw = NONE
+      posh = NONE if posh == MID_H
+    end
+    #-----拡大処理-----
+    if edge ==YOKO then
+      inx = (outw.to_f - inw.to_f) / 2.0
+      inw_tmp = inw if posw == RIGHT
+      inw = outw
+      iny = 0
+    elsif edge == TATE then
+      inx = 0
+      iny = (inh.to_f - outh.to_f) / 2.0
+      inh = outh
+    else
+      inx = 0
+      inw = outw
+      iny = (inh.to_f - outh.to_f) / 2.0
+      inh = outh
+    end
+      outx = outy = 0
+      ret = Hash.new
+      ret.default = 0
+      ret.store("edge", edge)
+      ret.store("inx", inx)
+      ret.store("inw", inw)
+      ret.store("outw", outw)
+      ret.store("iny", iny)
+      ret.store("inh", inh)
+      ret.store("outh", outh)
+      ret.store("outx", outx)
+      ret.store("outy", outy)
+      ret.store("inw_tmp", inw_tmp)
+      ret.store("posw", posw)
+      ret.store("posh", posh)
+    return ret
+  end
+
   def without_deform (inw, outw, inx, outx, inh, outh, outy, iny)
     if inw > outw then
       inx = (inw - outw) / 2.0
@@ -121,20 +122,18 @@ def priority_short (edge, posh, posw, inx, inw, outw, iny, inh, outh, outx, outy
     ret.store("outx", outx)
     return ret
   end
-  
-  def fill(posh, posw, inx, iny, outx, outy)
-    posh = posw = NONE
-    inx = iny = outx = outy = 0
+
+  def fill(inw, inh, outw, outh)
     ret = Hash.new
     ret.default = 0
-    ret.store("posh", posh)
-    ret.store("posw", posw)
-    ret.store("inx", inx)
-    ret.store("iny", iny)
-    ret.store("outx", outx)
-    ret.store("outy", outy)
+    ret.store("inw", inw)
+    ret.store("inh", inh)
+    ret.store("outw", outw)
+    ret.store("outh", outh)
     return ret
   end
+
+
 
   def calcsize (inw, inh, outw, outh, deform, posw, posh)
     #---------------縦長か横長か正方形か調べる---------------
@@ -167,22 +166,22 @@ def priority_short (edge, posh, posw, inx, inw, outw, iny, inh, outh, outx, outy
         ret = without_deform(inw, outw, inx, outx, inh, outh, outy, iny)
       #----------縦も横も合わせる----------
       when FILL then
-        ret = fill(posh, posw, inx, iny, outx, outy)
-      end
-      
-      
-      #----------横位置の処理----------
-      case ret["posw"]
-        when LEFT then
-          ret["inx"] = 0
-        when MID_W then
-        when RIGHT then
-          ret["inx"] = ret["inw_tmp"].abs - ret["outw"] 
-        when NONE then
-      end
+        ret = fill(inw, inh, outw, outh)
+    end
 
-      #----------縦位置の処理----------
-      case ret["posh"]
+
+    #----------横位置の処理----------
+    case ret["posw"]
+      when LEFT then
+        ret["inx"] = 0
+      when MID_W then
+      when RIGHT then
+        ret["inx"] = ret["inw_tmp"].abs - ret["outw"] 
+      when NONE then
+    end
+
+    #----------縦位置の処理----------
+    case ret["posh"]
       when UP then
         ret["iny"] = 0
       when MID_H then
@@ -190,59 +189,43 @@ def priority_short (edge, posh, posw, inx, inw, outw, iny, inh, outh, outx, outy
       when LOW then
         ret["outy"] = ret["inh"] - ret["outh"]
       when NONE then
-      end
+    end
       
-     #---------絶対値＆整数に変換----------
+   #---------絶対値＆整数に変換----------
 
-ret.store("inx", inx) if inx == "nil"
-ret.store("iny", iny) if iny == "nil"
-ret.store("inw", inw) if inw == "nil"
-ret.store("inh", inh) if inh == "nil"
-ret.store("outx", outx) if outx == "nil"
-ret.store("outy", outy) if outy == "nil"
-ret.store("outw", outw) if outw == "nil"
-ret.store("outh", outh) if outh == "nil"
-ret.store("edge", edge) if edge == "nil"
-#ret.store("inw_tmp", inw_tmp) if inw_tmp == "nil"
+    ret.store("inx", inx) if inx == "nil"
+    ret.store("iny", iny) if iny == "nil"
+    ret.store("inw", inw) if inw == "nil"
+    ret.store("inh", inh) if inh == "nil"
+    ret.store("outx", outx) if outx == "nil"
+    ret.store("outy", outy) if outy == "nil"
+    ret.store("outw", outw) if outw == "nil"
+    ret.store("outh", outh) if outh == "nil"
+    ret.store("edge", edge) if edge == "nil"
 
-ret.store("outw", 0) if ret["outw"] == "nil"
+    ret.store("outw", 0) if ret["outw"] == "nil"
 
-=begin
-     ret["inx"] = ret["inx"].abs
-     ret["iny"] = ret["iny"].abs
-     ret["inw"] = ret["inw"].abs
-     ret["inh"] = ret["inh"].abs
-     ret["outx"] = ret["outx"].abs
-     ret["outy"] = ret["outy"].abs
-     ret["outw"] = ret["outw"].abs
-     ret["outh"] = ret["outh"].abs
-     
-     ret["inx"] = ret["inx"].truncate
-     ret["iny"] = ret["iny"].truncate
-     ret["inw"] = ret["inw"].truncate
-     ret["inh"] = ret["inh"].truncate
-     ret["outx"] = ret["outx"].truncate
-     ret["outy"] = ret["outy"].truncate
-     ret["outw"] = ret["outw"].truncate
-     ret["outh"] = ret["outh"].truncate
-=end
-     
-       return ret
-end
-  
-=begin
-def init(canvas_width, canvas_height)
-canvas = Imlib2::Image.new(canvas_width, canvas_height)#キャンバス作成
+    ret["inx"] = ret["inx"].abs
+    ret["iny"] = ret["iny"].abs
+    ret["inw"] = ret["inw"].abs
+    ret["inh"] = ret["inh"].abs
+    ret["outx"] = ret["outx"].abs
+    ret["outy"] = ret["outy"].abs
+    ret["outw"] = ret["outw"].abs
+    ret["outh"] = ret["outh"].abs
 
-canvas.fill_rect [0, 0, canvas_width, canvas_height]#色を白に染める
+    ret["inx"] = ret["inx"].truncate
+    ret["iny"] = ret["iny"].truncate
+    ret["inw"] = ret["inw"].truncate
+    ret["inh"] = ret["inh"].truncate
+    ret["outx"] = ret["outx"].truncate
+    ret["outy"] = ret["outy"].truncate
+    ret["outw"] = ret["outw"].truncate
+    ret["outh"] = ret["outh"].truncate
 
-image = Imlib2::Image.load('yokonaga.png')#イメージロード
-
-dst, src =  calcsize(image.width, image.height, canvas_width, canvas_height)#サイズ計算
-src[:x] = 3
-canvas.blend_image!(image, src[:x], src[:y], src[:w], src[:h], dst[:x], dst[:y], dst[:w], dst[:h])
-
-canvas.save("/home/ttakahashi/resize/blended.png")#ファイルに書き出す
-end
-=end
+    ret.delete("inw_tmp") if ret.key?("inw_tmp")
+    ret.delete("posw") if ret.key?("posw")
+    ret.delete("posh") if ret.key?("posh")
+    return ret
+  end
 end

05cdb14bc97d45c74be66dcac09072aad5a622f1
ttakahashi <ttakahashi@up-frontier.jp>
2012-07-24 18:50:46 +0900

    modified test witdhout_deform_leftup, middle, rightdown

--- pic_zoomer_test.rb
+++ pic_zoomer_test.rb
@@ -345,13 +345,19 @@ canvas.fill_rect [0, 0, 480, 360]
 image = Imlib2::Image.load('tatenaga.png')
 canvas.blend_image!(image, ret["inx"], ret["iny"], ret["inw"], ret["inh"], ret["outx"], ret["outy"], ret["outw"], ret["outh"])
 canvas.save("/home/ttakahashi/Study-Ruby-Imlib2/pic/test_tate_without_deform_pos_rightdown.png")
-  
     assert_equal({"inx" => 0, "iny" => 391, "inw" => 312, "inh" => 360, "outx" => 168, "outy" => 0, "outw" => 312, "outh" => 360} , ret)
   end
 
   def test_tate_height_full_left
     ret = @obj.calcsize("inw" => 312, "inh" => 751, "outw" => 480, "outh" => 360, "deform" => Transform::HEIGHT_FULL, "posw" => Transform::LEFT, "posh" => Transform::NONE)
-
+    ret["inx"] = 0
+    ret["iny"] = 0
+    ret["inw"] = 312
+    ret["inh"] =  751
+    ret["outx"] = 165#(480-149) / 2 priory_long‚Æ‹¤—L
+    ret["outy"] = 0
+    ret["outw"] = 149
+    ret["outh"] = 360
 canvas = Imlib2::Image.new(480, 360)
 canvas.fill_rect [0, 0, 480, 360]
 image = Imlib2::Image.load('tatenaga.png')
